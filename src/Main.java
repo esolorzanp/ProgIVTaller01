720cef1e5cdc3788de36fcf6db07a522c1cff33c
esolorzanp <esolorzanop@estudiante.uniajc.edu.co>
2024-02-12 01:15:44 -0500

    *** Creación forma MainGui *** Se modifica clase Main para invocar forma principal MainGui

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import gui.MainGui;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainGui mainGui = new MainGui();
+        System.exit(0);
     }
 }
---
+++ src/gui/MainGui.java
@@ -0,0 +1,81 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MainGui extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JButton punto21Button;
+    private JButton punto25Button;
+    private JButton punto31Button;
+
+    public MainGui() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        punto21Button.addActionListener(e -> callPunto21());
+        punto25Button.addActionListener(e -> callPunto25());
+        punto31Button.addActionListener(e -> callPunto31());
+
+        this.setSize(300,300);
+        this.setLocationRelativeTo(this);
+        this.setVisible(true);
+    }
+
+    private void callPunto21() {
+        Punto21Gui punto21Gui = new Punto21Gui();
+    }
+
+    private void callPunto25() {
+        Punto25Gui punto25Gui = new Punto25Gui();
+    }
+
+    private void callPunto31() {
+        Punto31Gui punto31Gui = new Punto31Gui();
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        MainGui dialog = new MainGui();
+        dialog.pack();
+        System.exit(0);
+    }
+}
